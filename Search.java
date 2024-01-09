eb550743688e572ac7c4bd8d1d0ec4f16729405f
Venugopal K <venu.shastri@gmail.com>
2024-01-09 16:11:41 +0530

    Update Search.java

--- Search.java
+++ Search.java
@@ -2,12 +2,27 @@ import java.util.stream.Stream;
 import java.util.function.Predicate;
 
 public class Main {
+ public static Predicate<String> checkStringStartWithAny(string startString){
+    Predicate<String> preciateFunctionObj =str -> str.startsWith(startString);
+    return preciateFunctionObj;
+  }
+  
+  public static String[] filterItems(String[] items, Predicate<String> criteria) {
+      return Stream.of(items)
+              .filter(criteria)
+              .toArray(String[]::new);
+  }
+
+  
   public static void main(String[] args) {
       String[] items = {"Apple", "Car", "Toy", "Today"};
       
       // check for starting character
       String[] filteredStartswithItems = filterItems(items, str -> str.startsWith("T"));
-      
+       filteredStartswithItems = filterItems(items,checkStringStartWithAny("T"));
+       filteredStartswithItems = filterItems(items,checkStringStartWithAny("U"));
+    filteredStartswithItems = filterItems(items,checkStringStartWithAny("V"));
+    
       // check for length
       String[] filterLengthItems = filterItems(items, str -> str.length()>4);
       
@@ -15,7 +30,10 @@ public class Main {
     display(filteredStartswithItems,filterLengthItems);
    
   }
-  public static void display(String [] filteredStartswithItems,String [] filterLengthItems){
+ 
+
+ 
+ public static void display(String [] filteredStartswithItems,String [] filterLengthItems){
              System.out.println("String Items that start with T:-");
       for(String item: filteredStartswithItems){
           System.out.println(item);
@@ -27,10 +45,3 @@ public class Main {
           System.out.println(item);
       }
   }
-
-  public static String[] filterItems(String[] items, Predicate<String> criteria) {
-      return Stream.of(items)
-              .filter(criteria)
-              .toArray(String[]::new);
-  }
-}
