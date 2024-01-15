d18f05a8e4885af86dd3c63b168e960eca148b53
Abhinav Modi <abhinavmodi07@gmail.com>
2024-01-15 11:37:40 +0530

    Update Thread.java

--- Thread.java
+++ Thread.java
@@ -1,46 +1,76 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+import java.util.ArrayList;
 
 class Main {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(1);
-        t1.start();
-        t1.stopped();
-    }
+   public static void main(String[] args) {
+       Dashboard dashboard = new Dashboard();
+       Thread t1 = new Thread(1);
+       t1.subscribe(dashboard);
+       t1.start();
+       t1.stop();
+   }
 }
-class Thread{
-    int id;
-    String state;
-    String priority;
-    String culture;
-    
-    Thread(int id){
-        this.id = id;
-        this.state = "created";
-        this.priority = "default";
-        this.culture = "default";
-        System.out.println("Thread: "+this.id+" "+ this.state);
-    }
-    void start(){
-        this.state="started";
-        System.out.println("Thread: "+this.id+" "+ this.state);
-    }
+
+class Thread {
+   int id;
+   String state;
+   String priority;
+   String culture;
+   ArrayList<IObserver> observers;
+
+   Thread(int id) {
+       this.id = id;
+       this.state = "created";
+       this.priority = "default";
+       this.culture = "default";
+       System.out.println("Thread: " + this.id + " " + this.state);
+       observers = new ArrayList<>();
+   }
+
+   void start() {
+       this.state = "started";
+       notifyObservers();
+   }
+   
     void sleep(){
         this.state="sleep";
-        System.out.println("Thread: "+this.id+" "+ this.state);
+        notifyObservers();
     }
  
      void aborted(){
          this.state="aborted";
-        System.out.println("Thread: "+this.id+" "+ this.state);
+         notifyObservers();
     }
      void suspended(){
          this.state="suspended";
-        System.out.println("Thread: "+this.id+" "+ this.state);
+         notifyObservers();
     }
-      void stopped(){
-         this.state="stopped";
-        System.out.println("Thread: "+this.id+" "+ this.state);
-    }
-    
+   void stop() {
+       this.state = "stopped";
+       notifyObservers();
+   }
+
+   private void notifyObservers() {
+       for (IObserver observer : observers) {
+           observer.update(this.state);
+       }
+   }
+
+   void subscribe(IObserver observer) {
+       observers.add(observer);
+   }
+
+   void unsubscribe(IObserver observer) {
+       observers.remove(observer);
+   }
+}
+
+class Dashboard implements IObserver {
+   @Override
+   public void update(String state) {
+       System.out.println("Changed State of thread: " + state);
+   }
+}
+
+interface IObserver {
+   void update(String state);
 }
